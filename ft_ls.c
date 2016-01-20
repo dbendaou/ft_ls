87b540f67b4909b45ecf3fdc5745440f2fc26a02
dbendaou <dbendaou@student.42.fr>
2016-01-20 18:15:09 +0100

    Initial Commit

--- ft_ls.c
+++ ft_ls.c
@@ -6,8 +6,59 @@
 /*   By: dbendaou <dbendaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 16:32:31 by dbendaou          #+#    #+#             */
-/*   Updated: 2016/01/20 17:02:33 by dbendaou         ###   ########.fr       */
+/*   Updated: 2016/01/20 18:14:26 by dbendaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "ft_ls.h"
+
+int 	ft_ls(char *direct)
+{
+	char 			*path = NULL;
+	DIR 			*p_dir;
+	struct dirent	*s_dir;
+	t_dlst			*begin;
+	t_dlst			*tmp;
+	char 			*stmp;
+
+	stmp = ft_strjoin(direct, "/");
+	begin = (t_dlst *)malloc(sizeof(t_dlst));
+	begin->next = NULL;
+	begin->prev = NULL;
+	tmp = begin;
+	if (!(p_dir = opendir(direct)))
+		perror("");
+	while ((tmp->str = readdir(p_dir)))
+	{
+		tmp->next = (t_dlst *)malloc(sizeof(t_dlst));
+		tmp->next->prev = tmp;
+		tmp->next->next = NULL;
+		tmp->next->str = NULL;
+		tmp = tmp->next;
+	}
+	closedir(p_dir);
+	tmp = begin;
+	while (tmp->str)
+	{
+		printf("%s\n", tmp->str->d_name);
+		tmp = tmp->next;
+	}
+	tmp = begin;
+	while (tmp->str)
+	{
+		if (tmp->str->d_type == DT_DIR && tmp->str->d_name[0] != '.')
+		{
+			path = ft_strjoin(stmp, tmp->str->d_name);
+			printf("JE VEUX OUVRIR : %s\n",tmp->str->d_name);
+			ft_ls(path);
+		}
+
+		tmp = tmp->next;
+	}
+	return (1);
+	
+}
+int 	main(int ac, char **av)
+{
+	ft_ls(av[1]);
+}
--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: dbendaou <dbendaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/20 16:51:56 by dbendaou          #+#    #+#             */
-/*   Updated: 2016/01/20 17:02:36 by dbendaou         ###   ########.fr       */
+/*   Updated: 2016/01/20 18:14:28 by dbendaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,20 @@
 # include <sys/stat.h>
 # include <dirent.h>
 # include <unistd.h>
+# include <pwd.h>
+# include <grp.h>
+# include <errno.h>
+# include "../Libft-42/libft.h"
+# include <stdio.h>
+
+typedef struct 		s_dlst
+{
+	struct dirent	*str;
+	struct s_dlst	*next;
+	struct s_dlst	*prev;
+}					t_dlst;
+
+int		ft_ls(char *direct);
 
 
 #endif
